50b94a5c007b70ae8872589b8edc2616f5d15478
tony <342561618@qq.com>
2023-08-09 19:48:43 +0800

    修改配置文件名称

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import 'ant-design-vue/dist/reset.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { setupAntDesignVue } from './settings/ant-design-vue.setting'
+import { setupAntDesignVue } from './settings/ant-design-vue-setting'
 
 const app = createApp(App)
 
