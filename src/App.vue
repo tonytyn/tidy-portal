875a2e5518957f9f0f8e49490ec415391c8c8a53
tony <342561618@qq.com>
2023-09-04 23:22:37 +0800

    1.添加全局配置组件，控制组件的语言。2.修改样式，解决右侧内容变高以后左侧菜单栏没有跟着变高的问题

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+const locale = zhCN
 </script>
 <template>
-  <RouterView style="height: 100%" />
+  <a-config-provider :locale="locale">
+    <RouterView style="min-height: 100%" />
+  </a-config-provider>
 </template>
