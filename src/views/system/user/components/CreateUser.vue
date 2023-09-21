3468bc311f63de050f1857741a9e990ae7e9b9d1
tony <342561618@qq.com>
2023-09-21 23:11:56 +0800

    完善创建用户表单

--- src/views/system/user/components/CreateUser.vue
+++ src/views/system/user/components/CreateUser.vue
@@ -7,7 +7,10 @@ const emit = defineEmits(['modalClose'])
 
 const formRef = ref()
 const createUserParams = ref({
-  username: ''
+  username: '',
+  account: '',
+  password: '',
+  phone: ''
 })
 const handleSubmit = () => {
   formRef.value.resetFields()
@@ -22,18 +25,32 @@ const handleCancel = () => {
     :open="modalVisible"
     title="创建用户"
     cancelText="false"
-    
     @cancel="handleCancel"
     :destroyOnClose="true"
+    :footer="null"
   >
-    <a-form ref="formRef" layout="inline" :model="createUserParams">
+    <a-form
+      ref="formRef"
+      :model="createUserParams"
+      :label-col="{ span: 5 }"
+      :wrapper-col="{ span: 15 }"
+    >
       <a-form-item label="用户名" name="username">
         <a-input v-model:value="createUserParams.username"> </a-input>
       </a-form-item>
+      <a-form-item label="账号" name="account">
+        <a-input v-model:value="createUserParams.account"> </a-input>
+      </a-form-item>
+      <a-form-item label="密码" name="password">
+        <a-input-password  v-model:value="createUserParams.password"> </a-input-password>
+      </a-form-item>
+      <a-form-item label="手机号" name="phone">
+        <a-input v-model:value="createUserParams.phone"> </a-input>
+      </a-form-item>
+      <div style="text-align: center">
+        <a-button type="primary" @click="handleSubmit">提交</a-button>
+      </div>
     </a-form>
-    <template #footer>
-      <a-button type="primary" @click="handleSubmit">提交</a-button>
-    </template>
   </a-modal>
 </template>
 
