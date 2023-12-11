bb3df30aedc597859b17e8a90b2603a86b2c121b
tony <342561618@qq.com>
2023-12-11 22:09:28 +0800

    再次修改打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,20 @@ import type { UserConfig, ConfigEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteMockServe } from 'vite-plugin-mock'
 
+const noAttr = () => {
+  return {
+    name: 'no-attribute',
+    transformIndexHtml(html: string) {
+      return html.replace(`type="module" crossorigin`, '')
+    }
+  }
+}
 export default defineConfig(({ command }: ConfigEnv): UserConfig => {
-  
   return {
     base: './',
     plugins: [
       vue(),
+      noAttr(),
       viteMockServe({
         mockPath: 'mock',
         enable: command === 'serve',
