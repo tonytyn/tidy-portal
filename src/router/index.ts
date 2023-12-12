8467bae89326ee26f2c9858a1f40a6a829e593f9
tony <342561618@qq.com>
2023-12-12 20:20:52 +0800

    更改路由模式为传统html5模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import { AppstoreAddOutlined, CodeOutlined, SettingOutlined } from '@ant-design/icons-vue'
 
@@ -9,8 +9,7 @@ import Layout from '@/layout/index.vue'
  */
 
 const router = createRouter({
-  // history: createWebHistory(import.meta.env.BASE_URL),
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
