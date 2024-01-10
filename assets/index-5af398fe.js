155f589e5f1e55f2cf0a00a1659fb0471cf41f0f
tonytyn <48338846+tonytyn@users.noreply.github.com>
2024-01-10 12:45:18 +0000

    Deploying to gh-pages from @ tonytyn/tidy-portal@8c335ef9b4a5002d8116ab7dc21bc5a17b627011 🚀

---
+++ assets/index-5af398fe.js
@@ -0,0 +1,1 @@
+import{d as t,r as n,o as a,c as o,t as s}from"./index-2fbd72ca.js";const _=t({__name:"index",setup(r){const e=n("应用管理");return(c,l)=>(a(),o("div",null,s(e.value),1))}});export{_ as default};
