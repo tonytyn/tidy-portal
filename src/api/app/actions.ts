37415b851338902b374a7103fb85d20578baf8b2
tony <342561618@qq.com>
2024-01-10 21:33:35 +0800

    去掉api对象

--- src/api/app/actions.ts
+++ src/api/app/actions.ts
@@ -1,9 +1,6 @@
 import { defRequest } from '@/utils/request'
 import type { AppListModel } from '@/api/app/models'
 
-const AppApi = {
-  searchList: '/app/list'
-}
 export const searchAppList = () => {
-  return defRequest.get<ResponseResult<AppListModel[]>>(AppApi.searchList)
+  return defRequest.get<ResponseResult<AppListModel[]>>('/app/list')
 }
