c9a8bf50e28ffcf263fdc7fce7e83564c8dd8cc5
tony <342561618@qq.com>
2024-03-18 00:05:25 +0800

    添加生产环境的mock配置

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { setupAntDesignVue } from './settings/ant-design-vue-setting'
+import { setupProdMockServer } from './mockProdServer'; 
+
+setupProdMockServer(); 
 
 const app = createApp(App)
 
