ae32c3049e702a06cf97ca63056eacc5b606bd87
tony <342561618@qq.com>
2024-03-18 22:02:21 +0800

    vite-plugin-mock降级并修改配置。3.0还没支持线上使用，就先用2.0吧

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { setupAntDesignVue } from './settings/ant-design-vue-setting'
-import { setupProdMockServer } from './mockProdServer'; 
+import { setupProdMockServer } from './settings/mock-server-setting'; 
 
 setupProdMockServer(); 
 
 const app = createApp(App)
-
 setupAntDesignVue(app)
 
 app.use(router)
--- src/settings/mock-server-setting.ts
+++ src/settings/mock-server-setting.ts
@@ -1,14 +1,15 @@
 //  mockProdServer.ts
-import { createProdMockServer } from 'vite-plugin-mock/client'
+import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer'
 
 // 逐一导入您的mock.ts文件
 // 如果使用vite.mock.config.ts，只需直接导入文件
 // 可以使用 import.meta.glob功能来进行全部导入
-import app from '../mock/app'
-import menu from '../mock/menu'
-import role from '../mock/role'
-import user from '../mock/user'
+import app from '../../mock/app'
+import menu from '../../mock/menu'
+import role from '../../mock/role'
+import user from '../../mock/user'
 
 export function setupProdMockServer() {
   createProdMockServer([...app,...menu,...role,...user])
 }
+
